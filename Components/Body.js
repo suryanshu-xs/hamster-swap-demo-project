3d2b465d9fce9374e3c5cdf856e8070d544c3815
Suryanshu <suryanshusweetu12345@gmail.com>
2022-05-23 20:52:23 +0530

    first commit

---
+++ Components/Body.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Body = () => {
+  return (
+    <main>
+    
+    <h1>Body</h1>
+
+    </main>
+  )
+}
+
+export default Body
---
+++ Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <h3>Navbar</h3>
+    </nav>
+  )
+}
+
+export default Navbar
